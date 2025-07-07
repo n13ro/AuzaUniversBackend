8ad4432ab1720512433ff6d245211f73d9355b91
n13ro <danilbagisev472@gmail.com>
2025-07-07 09:51:04 +0700

    unit тесты
    модульные тесты на проверку опыта, и добавление коинов

---
+++ Tests/Unit/StudentCoinsTests.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Unit
+{
+    public class StudentCoinsTests
+    {
+        [Fact]
+        public void AddCoin_EqualCoin()
+        {
+            var student = new Student("Test", "T", "S", "test@mail.com", "123");
+            student.AddCoin(10);
+            Assert.Equal(10, student.CoinBalnce);
+        }
+
+    }
+}
--- Tests/Unit/StudentExperienceTests.cs
+++ Tests/Unit/StudentExperienceTests.cs
@@ -12,7 +12,7 @@ namespace Tests.Unit
         [Fact]
         public void AddXP_IncrXP()
         {
-            var student = new Student("Test", "T", "S", "test@mail.com", "123", 1);
+            var student = new Student("Test", "T", "S", "test@mail.com", "123");
             student.AddXP(100);
             Assert.Equal(100, student.XP);
         }
@@ -20,7 +20,7 @@ namespace Tests.Unit
         [Fact]
         public void AddXP_LevelUp1()
         {
-            var student = new Student("Test", "T", "S", "test@mail.com", "123", 1);
+            var student = new Student("Test", "T", "S", "test@mail.com", "123");
             student.AddXP(1200);
             Assert.Equal(200, student.XP);
             Assert.Equal(2, student.Level);
@@ -31,11 +31,23 @@ namespace Tests.Unit
         [Fact]
         public void AddXP_LevelUP2()
         {
-            var student = new Student("Test", "T", "S", "test@mail.com", "123", 1);
+            var student = new Student("Test", "T", "S", "test@mail.com", "123");
             student.AddXP(999);
             Assert.Equal(1, student.Level);
             student.AddXP(1);
             Assert.Equal(2, student.Level);
+
+        }
+
+        [Fact]
+        public void AddingExperience_ForAnAchievement()
+        {
+            var student = new Student("Test", "T", "S", "test@mail.com", "123");
+            var achiev1 = new Achievement("Aciev1", "adadadada", 1001);
+
+            achiev1.AssignToStudent(student);
+            Assert.Equal(1, student.XP);
+            Assert.Equal(1, student.Level);
 
         }
 
